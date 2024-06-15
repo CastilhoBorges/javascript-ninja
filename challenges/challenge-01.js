e73b0ad528188664b0bd2c7076d3e93fec43dd5a
CastilhoBorges <augustocastilhoborges21@gmail.com>
2024-06-15 09:21:56 -0300

    .

--- challenges/challenge-01.js
+++ challenges/challenge-01.js
@@ -32,10 +32,10 @@ soma === myvar // false
 myvar <= soma // true 
 
 // Crie uma função chamada `divisao` que receba como parâmetro dois números, e retorne o resultado da divisão entre eles.
-function divisão(a, b) {
+function divisao(a, b) {
     return a / b
 }
 
 // Invoque a função criada acima, passando os parâmetros 10 e 2.
-divisão(10, 2) // 5 
+divisao(10, 2) // 5 
 
