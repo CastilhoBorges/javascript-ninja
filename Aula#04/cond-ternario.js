ffdaefaa89dfa3708afa86f33ed814c134647198
CastilhoBorges <augustocastilhoborges21@gmail.com>
2024-06-19 13:40:50 -0300

    Condição Ternaria

---
+++ Aula#04/cond-ternario.js
@@ -0,0 +1,20 @@
+/*
+
+O Condicional ternario substiui o IF e o ELSE
+(CONDIÇÃO ? TRUE : FALSE) se for verdade ele para no bloco da ? se não cai no bloco do :
+
+*/
+
+var x = (1 === 2 ? true : false); 
+console.log(x) // false 
+
+// Se usace o padrão ia ficar bem maior 
+if (1 === 2) {
+    x = true
+} else {
+    x = false
+}
+
+
+// Usar para instruções pequenas 
+// Podemos usar tambem para conferir se o valor é false ou true 
