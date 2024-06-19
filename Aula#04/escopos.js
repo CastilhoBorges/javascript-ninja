24c9760324ec37f30408df58a6975bbb8968cdd6
CastilhoBorges <augustocastilhoborges21@gmail.com>
2024-06-19 13:54:29 -0300

    Comentarios

--- Aula#04/escopos.js
+++ Aula#04/escopos.js
@@ -6,11 +6,13 @@ Ja fora estara no escopo global.
 var myVar = 2 
 function myFunction() {
     return myVar; // Vai retornar o valor pois a myVar foi declarada do escopo global
-} ;
+};
 
 function otherFunction() {
     var otherVar = true;
-    return otherVar 
+    return otherVar;
 };
 
-console.log(otherVar) // Vai falar que não esta definida pois so definimos ela dentro da function 
+console.log(otherVar) // Vai falar que não esta definida pois so definimos ela dentro da function
+
+/* Argumentos de funções tambem ficam no escopo local */
